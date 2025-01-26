c974de930150b3ad344f7de1afc047af85331e2e
Melissa Cianfarano <mmead404@yahoo.com>
2025-01-26 16:16:39 -0500

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+// Read trips data from JSON file
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // GET travel view
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+const travel = (req, res) => {              // pass trips data into handlebar view
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
